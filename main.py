782e22d92137ec814925a40c7c3b62ade9baaa95
PR0FESS0R-99 <74817426+PR0FESS0R-99@users.noreply.github.com>
2021-09-26 20:19:27 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import os
 from pyrogram import Client, filters
-
+from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton
+from config import BOTUSERNAME
 
 bughunter0 = Client(
      os.environ.get("SESSION_NAME", "No-Forward-Messages"),
@@ -8,6 +9,26 @@ bughunter0 = Client(
      api_id = int(os.environ["API_ID"]),
      api_hash = os.environ["API_HASH"]
 )
+
+START_MSG = """👋Hey {}
+
+Simple Telegram Bot to Automatically delete Forwarded messages  in Group.
+
+Maintained By @BugHunterBots
+"""
+
+START_BUTTON = InlineKeyboardMarkup([[
+InlineKeyboardButton(text="➕️Add Me To Your Chat", url=f"http://t.me/{BOTUSERNAME}?startgroup=botstart")
+]]
+)
+
+@bughunter0.on_message(filters.private & filters.command("start"))
+async def start_handler(bot, update):
+    await update.reply_text(
+        text=f"""{START_MSG}""".format(update.from_user.mention),
+        disable_web_page_preview=True,
+        reply_markup=START_BUTTON
+  )
 
 
 @bughunter0.on_message(filters.forwarded)
