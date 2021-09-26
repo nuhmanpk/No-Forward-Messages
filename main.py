a63feb50c27e95f0752030ff15a74406cf38571b
Fayas Noushad <TheFayasNoushad@gmail.com>
2021-09-26 19:21:18 +0530

    Code formatted

--- main.py
+++ main.py
@@ -1,12 +1,14 @@
 import os
 from pyrogram import Client, filters
 
+
 bughunter0 = Client(
     "BotNameHere",
      bot_token = os.environ["BOT_TOKEN"],
      api_id = int(os.environ["API_ID"]),
      api_hash = os.environ["API_HASH"]
 )
+
 
 @bughunter0.on_message(filters.forwarded)
 async def forward(bot,message):
